3c281a881885de01acc556e02fb96805e29fcd36
Sejal Rai <sejalrai23@gmail.com>
2022-03-19 04:34:14 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const cors=require('cors');
+
+const authRoutes=require("./routes/auth.js");
+
+const app=express();
+const PORT=process.env.PORT || 4000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/',(req,res)=>{
+  res.send('Hello !')
+});
+
+app.use('/auth',authRoutes);
+
+app.listen(PORT , ()=>console.log(`Server is running on ${PORT}`));
